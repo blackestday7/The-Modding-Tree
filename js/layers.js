4813e8c14aa4a9e8eaee99a8e131b7519cbfffb4
blackestday7 <80119105+blackestday7@users.noreply.github.com>
2021-03-05 22:27:54 +0100

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -24,56 +24,6 @@ addLayer("a", {
     hotkeys: [
         {key: "a", description: "A: Reset for Attention spans", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
-    upgrades: {
-        rows: 2,
-        cols: 3,
-        11: {
-            title: "Generator of Genericness",
-            description: "Gain 1 Fuck every second.",
-            cost: new Decimal(1),
-            unlocked() { return player[this.layer].unlocked }, // The upgrade is only visible when this is true
-        },
-        12: {
-            description: "Point generation is faster based on your unspent Attention spans.",
-            cost: new Decimal(1),
-            unlocked() { return (hasUpgrade(this.layer, 11))},
-            effect() { // Calculate bonuses from the upgrade. Can return a single value or an object with multiple values
-                let ret = player[this.layer].points.add(1).pow(player[this.layer].upgrades.includes(24)?1.1:(player[this.layer].upgrades.includes(14)?0.75:0.5)) 
-                if (ret.gte("1e20000000")) ret = ret.sqrt().times("1e10000000")
-                return ret;
-            },
-            effectDisplay() { return format(this.effect())+"x" }, // Add formatting to the effect
-        },
-        13: {
-            unlocked() { return (hasUpgrade(this.layer, 12))},
-            onPurchase() { // This function triggers when the upgrade is purchased
-                player[this.layer].unlockOrder = 0
-            },
-            style() {
-                if (hasUpgrade(this.layer, this.id)) return {
-                'background-color': '#1111dd' 
-                }
-                else if (!canAffordUpgrade(this.layer, this.id)) {
-                    return {
-                        'background-color': '#dd1111' 
-                    }
-                } // Otherwise use the default
-            },
-            canAfford(){return player.points.lte(7)},
-            pay(){player.points = player.points.add(7)},
-            fullDisplay: "Only buyable with less than 7 points, and gives you 7 more. Unlocks a secret subtab."
-        },
-        22: {
-            title: "This upgrade doesn't exist",
-            description: "Or does it?.",
-            currencyLocation() {return player[this.layer].buyables}, // The object in player data that the currency is contained in
-            currencyDisplayName: "exhancers", // Use if using a nonstandard currency
-            currencyInternalName: 11, // Use if using a nonstandard currency
-
-            cost: new Decimal(3),
-            unlocked() { return player[this.layer].unlocked }, // The upgrade is only visible when this is true
-        },
-    }
 })  
 addLayer("p", {
     name: "pussy", // This is optional, only used in a few places, If absent it just uses the layer id.
