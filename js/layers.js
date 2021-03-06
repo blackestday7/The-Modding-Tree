f53f72060bafedf6bb7acf4a60e71c399c045d57
blackestday7 <80119105+blackestday7@users.noreply.github.com>
2021-03-06 11:35:00 +0100

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -34,11 +34,22 @@ addLayer("a", {
             unlocked() { return player[this.layer].unlocked }, // The upgrade is only visible when this is true
         },
         12: {
-            description: "Fuck generation is faster based on your unspent Attention span.",
+            description: "ADHD generation is faster based on your unspent Attention span.",
             cost: new Decimal(1),
             unlocked() { return (hasUpgrade(this.layer, 11))},
             effect() { // Calculate bonuses from the upgrade. Can return a single value or an object with multiple values
                 let ret = player[this.layer].points.add(1).pow(player[this.layer].upgrades.includes(24)?1.1:(player[this.layer].upgrades.includes(14)?0.75:0.5)) 
+                if (ret.gte("1e20000000")) ret = ret.sqrt().times("1e10000000")
+                return ret;
+            },
+            effectDisplay() { return format(this.effect())+"x" }, // Add formatting to the effect
+        },
+        13: {
+            description: "ADHD generation is even faster based on your unspent Attention span.",
+            cost: new Decimal(5),
+            unlocked() { return (hasUpgrade(this.layer, 11))},
+            effect() { // Calculate bonuses from the upgrade. Can return a single value or an object with multiple values
+                let ret = player[this.layer].points.add(2).pow(player[this.layer].upgrades.includes(24)?1.1:(player[this.layer].upgrades.includes(14)?0.75:0.5)) 
                 if (ret.gte("1e20000000")) ret = ret.sqrt().times("1e10000000")
                 return ret;
             },
