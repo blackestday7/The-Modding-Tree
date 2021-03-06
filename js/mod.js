2ea459dbe8cfe6368794bd0fefd30da6ce96a942
blackestday7 <80119105+blackestday7@users.noreply.github.com>
2021-03-06 11:39:34 +0100

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -43,6 +43,7 @@ function getPointGen() {
 
 	let gain = new Decimal(1)
 	if (hasUpgrade("a", 12)) gain = gain.times(upgradeEffect("a", 12))
+	if (hasUpgrade("a", 13)) gain = gain.times(upgradeEffect("a", 13))
 	return gain
 }
 
