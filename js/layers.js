82f77b7215e61e9bb1df75dc048d1f0a39c7b67a
blackestday7 <80119105+blackestday7@users.noreply.github.com>
2021-03-06 11:36:28 +0100

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -49,7 +49,7 @@ addLayer("a", {
             cost: new Decimal(5),
             unlocked() { return (hasUpgrade(this.layer, 12))},
             effect() { // Calculate bonuses from the upgrade. Can return a single value or an object with multiple values
-                let ret = player[this.layer].points.add(2).pow(player[this.layer].upgrades.includes(24)?1.1:(player[this.layer].upgrades.includes(14)?0.75:0.5)) 
+                let ret = player[this.layer].points.add(1).pow(player[this.layer].upgrades.includes(24)?1.4:(player[this.layer].upgrades.includes(14)?0.75:0.5)) 
                 if (ret.gte("1e20000000")) ret = ret.sqrt().times("1e10000000")
                 return ret;
             },
