4b010f95a4c57438fac7f88b91331381b6737b55
blackestday7 <80119105+blackestday7@users.noreply.github.com>
2021-03-06 22:12:19 +0100

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -169,6 +169,19 @@ addLayer("h", {
             effect() { // Calculate bonuses from the upgrade. Can return a single value or an object with multiple values
                 let ret = player[this.layer].points.add(1).pow(player[this.layer].upgrades.includes(24)?1.1:(player[this.layer].upgrades.includes(14)?0.175:0.125)) 
                 if (ret.gte("1e20000000")) ret = ret.sqrt().times("1e10000000")
+                if (hasUpgrade("h", 12)) ret = ret.times(upgradeEffect("h", 12))
+                return ret;
+            },
+            effectDisplay() { return format(this.effect())+"^" }, // Add formatting to the effect
+        },
+        12: {
+            title: "Where was i?",
+            description: "Doubles the effect of the previous upgrade",
+            cost: new Decimal(6),
+            unlocked: true,
+            effect() { // Calculate bonuses from the upgrade. Can return a single value or an object with multiple values
+                let ret = 2 
+                if (ret.gte("1e20000000")) ret = ret.sqrt().times("1e10000000")
                 return ret;
             },
             effectDisplay() { return format(this.effect())+"^" }, // Add formatting to the effect
