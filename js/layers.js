0e951f3da1630d6546dd73dcbaa7b7596d80bb76
blackestday7 <80119105+blackestday7@users.noreply.github.com>
2021-03-07 14:16:15 +0100

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -302,3 +302,36 @@ addLayer("d", {
         },
     }
 })
+addLayer("p", {
+    name: "Prestige", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "P", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: false,
+		points: new Decimal(0),
+    }},
+    color: "#390273",
+    requires: new Decimal(1e9), // Can be a function that takes requirement increases into account
+    resource: "Doggo's", // Name of prestige currency
+    baseResource: "Attention spans", // Name of resource prestige is based on
+    baseAmount() {return player["a"].points}, // Get the current amount of baseResource
+    type: "static", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 0.8, // Prestige currency exponent
+    base: 3,
+    canBuyMax() {return hasMilestone("d", 1)},
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        mult = new Decimal(1)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
+    row: 2, // Row the layer is in on the tree (0 is the first row)
+    branches: ["a"],
+    hotkeys: [
+        {key: "d", description: "D: Reset for Doggo's", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+    ],
+    tooltipLocked() { // Optional, tooltip displays when the layer is locked
+        return ("HA! Nice try, but you won't get past here unless you have" + this.requires() + " Hyperfixations. And you only have " + formatWhole(player.points))
+    },
+})
